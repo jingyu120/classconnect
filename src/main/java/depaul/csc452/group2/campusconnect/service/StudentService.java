07c43f91261e255dcfa8979120044080c321a15a
Justin <jingyu120@gmail.com>
2021-11-11 21:16:36 -0600

    implement delete student

--- src/main/java/depaul/csc452/group2/campusconnect/service/StudentService.java
+++ src/main/java/depaul/csc452/group2/campusconnect/service/StudentService.java
@@ -3,9 +3,11 @@ package depaul.csc452.group2.campusconnect.service;
 import depaul.csc452.group2.campusconnect.model.Student;
 import depaul.csc452.group2.campusconnect.repo.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -18,5 +20,20 @@ public class StudentService {
 
     public void addStudent(Student student) {
         studentRepository.save(student);
+    }
+
+    public Student getStudentByID(long id) {
+        Optional<Student> optional = studentRepository.findById(id);
+        Student student = null;
+        if (optional.isPresent()) {
+            student = optional.get();
+        } else {
+            throw new RuntimeException("Student not found for ID:" + id);
+        }
+        return student;
+    }
+
+    public void deleteStudent(long id) {
+        studentRepository.deleteById(id);
     }
 }
