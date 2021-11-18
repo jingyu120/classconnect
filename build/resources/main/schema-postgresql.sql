7c27b79b055bcc73be78cb762916ac154aa02fc7
Justin Zhang <jingyu120@gmail.com>
2021-11-17 21:22:35 -0600

    fixed bug for gender

--- build/resources/main/schema-postgresql.sql
+++ build/resources/main/schema-postgresql.sql
@@ -1,7 +1,7 @@
-drop table if exists student_course;
-DROP TABLE IF EXISTS students;
+-- DROP TABLE IF EXISTS users_roles;
+-- DROP TABLE IF EXISTS roles;
+-- DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS courses;
-
 
 CREATE TABLE courses (
     id serial PRIMARY KEY,
@@ -9,14 +9,4 @@ CREATE TABLE courses (
     num int,
     nm VARCHAR(100),
     descrip VARCHAR(1000)
-);
-
-CREATE TABLE students (
-    id serial PRIMARY KEY,
-    usr_id VARCHAR(10),
-    nm VARCHAR(50),
-    email VARCHAR(50),
-    admitted_date VARCHAR(50),
-    major VARCHAR(50),
-    minor VARCHAR(50)
 );
